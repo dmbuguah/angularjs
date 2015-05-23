a41bfe17e4bff752f656700f469d5dbb889fb227
CDanM <danmbugua74@gmail.com>
2015-05-23 20:27:37 +0300

    Two way data binding.
    Changes in the model are automatically reflected on the view.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,13 +8,17 @@ phonecatApp.controller('PhoneListCtrl', function ($scope) {
 $scope.phones = [
   {'name': 'Nexus S',
    'snippet': 'Fast just got faster with Nexus S',
-   'size': '5.5 inch'},
+   'size': '5.5 inch',
+   'age':4},
 
   {'name': 'Motorola XOOM™ with Wi-Fi',
    'snippet': 'The Next, Next Generation tablet',
-    'size':'5.0 inch'},
+    'size':'5.0 inch',
+   'age':2},
 
   {'name': 'MOTOROLA XOOM™',
-   'snippet': 'The Next, Next Generation tablet.'}
+   'snippet': 'The Next, Next Generation tablet',
+    'age':3}
 ];
+$scope.orderProp='age';
 });
