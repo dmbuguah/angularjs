ca04818d150fb758812a3efef40f43014b7b01c1
CDanM <danmbugua74@gmail.com>
2015-05-23 19:24:53 +0300

    Changed index.html to include the search feature.
    Filters content according to inputs in the search input area.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,12 +5,16 @@
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.'},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.'},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.'}
-  ];
+$scope.phones = [
+  {'name': 'Nexus S',
+   'snippet': 'Fast just got faster with Nexus S',
+   'size': '5.5 inch'},
+
+  {'name': 'Motorola XOOM™ with Wi-Fi',
+   'snippet': 'The Next, Next Generation tablet',
+    'size':'5.0 inch'},
+
+  {'name': 'MOTOROLA XOOM™',
+   'snippet': 'The Next, Next Generation tablet.'}
+];
 });
