12995169b6000c532fd00587475bc22dc6611206
CDanM <danmbugua74@gmail.com>
2015-05-24 11:36:37 +0300

    Event handling.
    Thumbnail images when clicked replace the large image.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,9 +13,14 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
     $scope.orderProp = 'age';
   }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+pphonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
     $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
       $scope.phone = data;
+      $scope.mainImageUrl = data.images[0];
     });
+
+    $scope.setImage = function(imageUrl) {
+      $scope.mainImageUrl = imageUrl;
+    };
   }]);
